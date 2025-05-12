9336859108d137b2d558b87db2bd140bcf73e0dd
lokeshklu <lokeshchowdary957@gmail.com>
2025-05-12 01:46:07 -0700

    removed outer folder

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "🚀 Installing dependencies..."
 pip install --upgrade pip
 pip install -r django_backend/requirements.txt
 
-export DJANGO_SETTINGS_MODULE=django_backend.django_backend.settings
+export DJANGO_SETTINGS_MODULE=django_backend.settings
 echo "Using DJANGO_SETTINGS_MODULE: [$DJANGO_SETTINGS_MODULE]"
 
 echo "⚙️ Applying migrations..."
